3f94920c8bacae275b0f08814e463147f785785f
CPAS\STripal <stepan.tripal@ceskapojistovna.cz>
2017-10-19 08:43:27 +0200

    db testing

--- WebAPI/WebAPI2/Controllers/ConfController.cs
+++ WebAPI/WebAPI2/Controllers/ConfController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,6 +10,38 @@ namespace WebAPI2.Controllers
 {
     public class ConfController : ApiController
     {
+        [HttpGet]
+        public void GetAll()
+        {
+            var cs = $"Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password = myPassword; ";
 
+            var server = "c228wq.forpsi.com";
+            var database = "f96194";
+            var uid = "f96194";
+            var password = "8VeqtaX";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            var connection = new SqlConnection(connectionString);
+
+
+
+
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("Select id from test where name=@zip", conn);
+            // int result = command.ExecuteNonQuery();
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    Console.WriteLine(String.Format("{0}", reader["id"]));
+                }
+            }
+
+            conn.Close();
+
+        }
     }
 }
